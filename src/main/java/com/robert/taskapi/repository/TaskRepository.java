97f2e9256a0ed115aa292827926de12894e2d8f7
Nismoe <158603338+Nismoe@users.noreply.github.com>
2025-03-23 15:40:03 +0100

    Delete src directory

--- src/main/java/com/robert/taskapi/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.robert.taskapi.repository;
-
-import com.robert.taskapi.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
